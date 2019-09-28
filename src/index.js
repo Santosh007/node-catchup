bd7133160cbc7c7e690e33437c11230efd51c691
Santosh <santoshreddy8341@live.com>
2019-09-28 15:40:19 +0530

    Added express
    Modified the project structure and added express js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-var http = require("http");
-var fs = require("fs");
+var express = require("express");
+var path = require("path");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    fs.readFile("src/index.html", function(err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-    //res.write("Hello World!"); //write a response to the client
-    //res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(8080);
