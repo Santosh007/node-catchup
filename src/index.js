733ff997f3957bf66b8a81bf393485bc3d646977
Santosh <santoshreddy8341@live.com>
2019-09-30 16:55:14 +0530

    Nodemon
    Added Nodemon as dev dependency

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var path = require("path");
+var http = require("http");
+const net = require("net");
 
 const app = express();
+const clients = [];
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(8080);
+http.createServer(app).listen(8080);
+
+//app.listen(8080);
+/*
+let client  = net.createConnection({port : 8080}, () => {
+  console.log("Clinet joined!");
+
+})
+*/
